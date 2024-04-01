745ee766a49ceeb5f09126d1b7549e5f8af0233f
Kyssuber <k22cong@siena.edu>
2024-04-01 10:53:58 -0500

    init commit

---
+++ pixelate.py
@@ -0,0 +1,94 @@
+import sys
+from matplotlib import pyplot as plt
+import numpy as np
+from PIL import Image
+
+import os
+homedir = os.getenv("HOME")
+
+class main_image:
+    
+    def __init__(self, im_path=None, npixels_x=55, offset=0.35):
+        self.im_path = im_path
+        
+        self.img_only = Image.open(self.im_path)
+        self.img_array = np.asarray(self.img_only)
+
+        self.height = np.shape(self.img_array)[0]
+        self.width = np.shape(self.img_array)[1]
+        
+        self.npixels_x = int(npixels_x)
+        
+        self.offset = float(offset)
+        
+    def get_scaling_fraction(self):
+
+        if self.height>self.width:
+            fraction = self.width/self.height
+            return 1, fraction
+        elif self.width>self.height:
+            fraction = self.height/self.width
+            return fraction, 1
+        elif self.width==self.height:
+            return  1, 1
+        else:
+            print("I don't know what to tell ye. Your width and height are not numbers.")
+            return None
+    
+    def scale_image(self):
+        
+        #this function is mine; it helps ensure that pixel cells are square- and not rectangular-shaped
+        frac_h, frac_w = self.get_scaling_fraction()
+
+        #resize smoothly down to desired number of pixels for x (55*frac_w) and y (55*frac_h)
+        self.img_scaled = self.img_only.resize((int(self.npixels_x*frac_w),int(self.npixels_x*frac_h)), 
+                                               resample=Image.Resampling.BILINEAR)
+        self.img_scaled_array = np.asarray(self.img_scaled)
+        
+    def add_grid(self):
+     
+        for n in range(np.shape(self.img_scaled_array)[0]):
+            plt.axhline(n+self.offset,lw=1,color='black',alpha=0.4)
+        for n in range(np.shape(self.img_scaled_array)[1]):
+            plt.axvline(n+self.offset,lw=1,color='black',alpha=0.4)  
+            
+    def plot_fig(self, savefig=False):
+        im_path_split = self.im_path.split('/')[-1]
+        im_name = im_path_split.split('.')
+        
+        plt.imshow(self.img_scaled_array)
+        self.add_grid()
+        plt.savefig(f'{homedir}/Desktop/{im_name[0]}_pxd.png',bbox_inches='tight',
+                    pad_inches=0.2,dpi=200)
+        
+        print('Image saved to Desktop.')
+        
+    def run_all_func(self):
+        self.get_scaling_fraction()
+        self.scale_image()
+        self.plot_fig()
+        
+        
+if __name__ == '__main__':    
+
+    if '-h' in sys.argv or '--help' in sys.argv:
+        print("Usage: %s [-im_path full_path_to_image] [-nx number_of_pixels_along_xaxis_(integer)] [-grid_offset optional_grid_offset_(float)]")
+        sys.exit(1)
+    
+    if '-im_path' in sys.argv:
+        p = sys.argv.index('-im_path')
+        im_path = str(sys.argv[p+1])
+        
+    if '-nx' in sys.argv:
+        p = sys.argv.index('-nx')
+        npixels_x = sys.argv[p+1]
+        
+    if '-grid_offset' in sys.argv:
+        p = sys.argv.index('-grid_offset')
+        offset = sys.argv[p+1]
+    else:
+        offset = 0.5
+        
+    img_class = main_image(im_path=im_path, npixels_x=npixels_x, offset=offset)
+    img_class.run_all_func()
+    
