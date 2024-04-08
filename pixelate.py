cc40777b12d83f03fea6558e8f2a996a1367a759
Kyssuber <k22cong@siena.edu>
2024-04-08 18:56:34 -0500

    more updates

--- pixelate.py
+++ pixelate.py
@@ -3,13 +3,14 @@ from matplotlib import pyplot as plt
 import matplotlib.ticker as ticker
 import numpy as np
 from PIL import Image
+import cv2
 
 import os
 homedir = os.getenv("HOME")
 
 class main_image:
     
-    def __init__(self, im_path=None, npixels_x=55, offset=0.35, sharpen=False):
+    def __init__(self, im_path=None, npixels_x=55, offset=0.35):
         self.im_path = im_path
         
         self.img_only = Image.open(self.im_path)
@@ -20,7 +21,6 @@ class main_image:
         
         self.npixels_x = int(npixels_x)
         self.offset = float(offset)
-        self.sharpen = bool(sharpen)
         
     def get_scaling_fraction(self):
 
@@ -46,40 +46,40 @@ class main_image:
                                                resample=Image.Resampling.BILINEAR)
         self.img_scaled_array = np.asarray(self.img_scaled)
         
-        if self.sharpen:
-            self.sharpen_image()
+        self.sharpen_image()
 
     def sharpen_image(self):
         #create the sharpening kernel 
         kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]]) 
         #sharpen the image 
-        self.img_scaled_array = cv2.filter2D(self.img_scaled_array, -1, kernel)   
+        self.img_scaled_array_sharp = cv2.filter2D(self.img_scaled_array, -1, kernel)   
                     
-    def add_grid(self):
+    def add_grid(self,im,axes):
      
         self.xticks = []
         self.yticks = []
         self.xlabels = []
         self.ylabels = []
         
-        for n in range(np.shape(self.img_scaled_array)[0]):
-            plt.axhline(n+self.offset,lw=1,color='black',alpha=0.4)
+        for n in range(np.shape(im)[0]):
+            axes.axhline(n+self.offset,lw=1,color='black',alpha=0.4)
             if n==0:
                 self.yticks.append(n-self.offset)
                 self.ylabels.append(n)
-            if (n+1)%5==0:
+            if (n+1)%10==0:
                 self.yticks.append(n+self.offset)
                 self.ylabels.append(n+1)
                      
-        for n in range(np.shape(self.img_scaled_array)[1]):
-            plt.axvline(n+self.offset,lw=1,color='black',alpha=0.4)  
+        for n in range(np.shape(im)[1]):
+            axes.axvline(n+self.offset,lw=1,color='black',alpha=0.3)  
             if n==0:
                 self.xticks.append(n-self.offset)
                 self.xlabels.append(n)
-            if (n+1)%5==0:
+            if (n+1)%10==0:
                 self.xticks.append(n+self.offset)
                 self.xlabels.append(n+1)         
     
+    '''
     def plot_fig(self, savefig=False):
         im_path_split = self.im_path.split('/')[-1]
         im_name = im_path_split.split('.')
@@ -89,16 +89,47 @@ class main_image:
         #flipping image and changing axis origin --> ensures y-axis increases from bottom to top
         #adapted from https://stackoverflow.com/questions/56916638/invert-the-y-axis-of-an-image-without-flipping-the-image-upside-down
         plt.imshow(np.flipud(self.img_scaled_array), origin='lower')
-        self.add_grid()
+        self.add_grid(im=self.img_scaled_array)
         
         ax.set_xticks(self.xticks,labels=self.xlabels)
         ax.set_yticks(self.yticks,labels=self.ylabels)  
         
-        plt.savefig(f'{homedir}/Desktop/{im_name[0]}_pxd.png',bbox_inches='tight',
+        if self.sharpen:
+            plt.savefig(f'{homedir}/Desktop/{im_name[0]}_pxd_sharp.png',bbox_inches='tight',
+                    pad_inches=0.2,dpi=200)
+        else:    
+            plt.savefig(f'{homedir}/Desktop/{im_name[0]}_pxd.png',bbox_inches='tight',
                     pad_inches=0.2,dpi=200)
         
         print('Image saved to Desktop.')
+    '''
+    
+    def plot_fig(self, savefig=False):
+        im_path_split = self.im_path.split('/')[-1]
+        im_name = im_path_split.split('.')
         
+        images = [self.img_scaled_array,self.img_scaled_array_sharp]
+        titles = ['Pixelated Image','Pixelated Image (Sharpened)']
+        
+        fig, (ax1,ax2) = plt.subplots(1,2, figsize=(20,10))
+        fig.subplots_adjust(wspace=0.1)
+        
+        for i in range(len(images)):
+            ax1.imshow(np.flipud(self.img_scaled_array), origin='lower')
+            self.add_grid(im=self.img_scaled_array,axes=ax1)
+            ax1.set_xticks(self.xticks,labels=self.xlabels)
+            ax1.set_yticks(self.yticks,labels=self.ylabels) 
+            ax2.imshow(np.flipud(self.img_scaled_array_sharp), origin='lower')
+            self.add_grid(im=self.img_scaled_array_sharp,axes=ax2)
+            ax2.set_xticks(self.xticks,labels=self.xlabels)
+            ax2.set_yticks(self.yticks,labels=self.ylabels) 
+            
+        plt.savefig(f'{homedir}/Desktop/{im_name[0]}_pxd_sharp.png',bbox_inches='tight',
+                    pad_inches=0.2,dpi=200)    
+        
+        print('Image saved to Desktop.')
+    
+    
     def run_all_func(self):
         self.get_scaling_fraction()
         self.scale_image()
@@ -107,7 +138,7 @@ class main_image:
 if __name__ == '__main__':    
 
     if '-h' in sys.argv or '--help' in sys.argv:
-        print("Usage: %s [-im_path full_path_to_image] [-nx number_of_pixels_along_xaxis_(integer)] [-grid_offset optional_grid_offset_(float)] [-sharpen (will use k-means clustering to sharpen foreground outline)]")
+        print("Usage: %s [-im_path full_path_to_image] [-nx number_of_pixels_along_xaxis_(integer)] [-grid_offset optional_grid_offset_(float)]")
         sys.exit(1)
     
     if '-im_path' in sys.argv:
@@ -124,20 +155,15 @@ if __name__ == '__main__':
     else:
         offset = 0.5
         
-    if '-sharpen' in sys.argv:
-        import cv2
-        sharpen=True
-    else:
-        sharpen=False
-        
-    img_class = main_image(im_path=im_path, npixels_x=npixels_x, offset=offset, sharpen=sharpen)
+    img_class = main_image(im_path=im_path, npixels_x=npixels_x, offset=offset)
     img_class.run_all_func()
     print('''
     
     DISCLAIMER NOTES:
         
-        --- Be sure to use the original image as a color reference. Indeed, the
-            -sharpen argument might produce some discoloration.
+        --- Be sure to use the original image or even the leftmost panel as a color 
+            reference. Indeed, the sharpening (right image panel) might produce 
+            some discoloration.
         
         --- The author does not recommend feeding in an already "pixelated" image
             into the program, as the pixel colors will not be as well-defined as 
